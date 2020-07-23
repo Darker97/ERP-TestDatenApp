b3200fe5f5f717fc2f7edefd9153fcb6f38c91c6
Christian - Darker97 <christian@baltzer.de>
2020-07-23 11:00:48 +0200

    Initial Commit

---
+++ ERP-TestRechner/ERP_TestRechnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ERP_TestRechnerApp.swift
+//  ERP-TestRechner
+//
+//  Created by Christian Baltzer on 23.07.20.
+//
+
+import SwiftUI
+
+@main
+struct ERP_TestRechnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
